46898b0ec877ef81d347f5b1ec3fe92c1e63b727
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2015-02-05 12:59:50 +0100

    let SoccerGame implement GameDataClient

--- src/main/java/foo/bar/services/external/SoccerGame.java
+++ src/main/java/foo/bar/services/external/SoccerGame.java
@@ -1,4 +1,23 @@
 package foo.bar.services.external;
 
-public class SoccerGame {
+import foo.bar.app.GameDataClient;
+
+public class SoccerGame implements GameDataClient{
+    @Override
+    public int getGoalsTeamA() {
+        //TODO to implement
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getGoalsTeamB() {
+        //TODO to implement
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getCurrentMinute() {
+        //TODO to implement
+        throw new UnsupportedOperationException();
+    }
 }

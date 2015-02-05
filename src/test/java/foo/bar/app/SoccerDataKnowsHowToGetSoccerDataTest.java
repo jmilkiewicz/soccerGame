1e12d6b0a8f4231321a8d92fd8091eadab31458c
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2015-02-05 12:58:47 +0100

    test Refactor

--- src/test/java/foo/bar/app/SoccerDataKnowsHowToGetSoccerDataTest.java
+++ src/test/java/foo/bar/app/SoccerDataKnowsHowToGetSoccerDataTest.java
@@ -1,5 +1,6 @@
 package foo.bar.app;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -9,37 +10,41 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class SoccerDataKnowsHowToGetSoccerDataTest {
+    public static final int A_TEAM_GOALS = 3;
+    public static final int B_TEAM_GOALS = 1;
+    public static final int CURRENT_MINUTE = 77;
     private GameDataClient gameDataClientMock = Mockito.mock(GameDataClient.class);
     private SoccerDataObject soccerDataObject = new SoccerDataObject(gameDataClientMock);
 
 
+    @Before
+    public void setUp() throws Exception {
+        when(gameDataClientMock.getGoalsTeamA()).thenReturn(A_TEAM_GOALS);
+        when(gameDataClientMock.getGoalsTeamB()).thenReturn(B_TEAM_GOALS);
+        when(gameDataClientMock.getCurrentMinute()).thenReturn(CURRENT_MINUTE);
+    }
+
     @Test
     public void shouldFetchGoalOfTeamsA() throws Exception {
-        when(gameDataClientMock.getGoalsTeamA()).thenReturn(3);
-
         int goalsTeamA = soccerDataObject.getGoalsTeamA();
 
-        assertThat(goalsTeamA, is(3));
+        assertThat(goalsTeamA, is(A_TEAM_GOALS));
         verify(gameDataClientMock).getGoalsTeamA();
     }
 
     @Test
     public void shouldFetchGoalOfTeamsB() throws Exception {
-        when(gameDataClientMock.getGoalsTeamB()).thenReturn(1);
-
         int goalsTeamB = soccerDataObject.getGoalsTeamB();
 
-        assertThat(goalsTeamB, is(1));
+        assertThat(goalsTeamB, is(B_TEAM_GOALS));
         verify(gameDataClientMock).getGoalsTeamB();
     }
 
     @Test
     public void shouldFetchCurrentMinute() throws Exception {
-        when(gameDataClientMock.getCurrentMinute()).thenReturn(77);
-
         int goalsTeamB = soccerDataObject.getCurrentMinute();
 
-        assertThat(goalsTeamB, is(77));
+        assertThat(goalsTeamB, is(CURRENT_MINUTE));
         verify(gameDataClientMock).getCurrentMinute();
     }
 }

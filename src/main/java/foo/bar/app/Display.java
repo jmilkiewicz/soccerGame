a2471850e7be6e15675e8bf836fc13efa0e97e34
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2015-02-05 11:05:23 +0100

    introducing Display interface

---
+++ src/main/java/foo/bar/app/Display.java
@@ -0,0 +1,12 @@
+package foo.bar.app;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: milus
+ * Date: 2/5/15
+ * Time: 11:01 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Display {
+    void onMeasurementsChanged();
+}

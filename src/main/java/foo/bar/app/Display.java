782a6e793ef1aed4dcd3fb7e0bc95b9de0508606
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2015-02-05 13:48:11 +0100

    removing unnecessary comment crap

--- src/main/java/foo/bar/app/Display.java
+++ src/main/java/foo/bar/app/Display.java
@@ -1,12 +1,5 @@
 package foo.bar.app;
 
-/**
- * Created with IntelliJ IDEA.
- * User: milus
- * Date: 2/5/15
- * Time: 11:01 AM
- * To change this template use File | Settings | File Templates.
- */
 public interface Display {
     void onMeasurementsChanged(SoccerDataObject soccerDataObject);
 }

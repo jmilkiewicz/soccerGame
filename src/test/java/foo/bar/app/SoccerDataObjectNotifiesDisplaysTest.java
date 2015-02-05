fc4176701469ff03808c14c532ac991ae7115a0f
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2015-02-05 11:19:32 +0100

    test to check if multiple displays are notified

--- src/test/java/foo/bar/app/SoccerDataObjectNotifiesDisplaysTest.java
+++ src/test/java/foo/bar/app/SoccerDataObjectNotifiesDisplaysTest.java
@@ -8,6 +8,8 @@ import static org.mockito.Mockito.verify;
 
 public class SoccerDataObjectNotifiesDisplaysTest {
     private Display mockDisplay = Mockito.mock(Display.class);
+    private Display mockDisplay2 = Mockito.mock(Display.class);
+    private Display mockDisplay3 = Mockito.mock(Display.class);
 
     @Test
     public void shallNotifyASingleDisplayOnMeasurementsChanged() throws Exception {
@@ -17,6 +19,21 @@ public class SoccerDataObjectNotifiesDisplaysTest {
         soccerDataObject.measurementsChanged();
 
         verify(mockDisplay).onMeasurementsChanged(soccerDataObject);
+    }
+
+
+    @Test
+    public void shallNotifyAMultipleDisplayOnMeasurementsChanged() throws Exception {
+        SoccerDataObject soccerDataObject = new SoccerDataObject();
+        soccerDataObject.appendDisplay(mockDisplay);
+        soccerDataObject.appendDisplay(mockDisplay2);
+        soccerDataObject.appendDisplay(mockDisplay3);
+
+        soccerDataObject.measurementsChanged();
+
+        verify(mockDisplay).onMeasurementsChanged(soccerDataObject);
+        verify(mockDisplay2).onMeasurementsChanged(soccerDataObject);
+        verify(mockDisplay3).onMeasurementsChanged(soccerDataObject);
     }
 
 }

9a0f454cbfa6ae4a59b1b12bcb6c6bacea60dd3c
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2015-02-05 11:37:58 +0100

    a new test to validate that all observers are called even if one of them throws exception

--- src/test/java/foo/bar/app/SoccerDataObjectNotifiesDisplaysTest.java
+++ src/test/java/foo/bar/app/SoccerDataObjectNotifiesDisplaysTest.java
@@ -2,14 +2,17 @@ package foo.bar.app;
 
 
 import org.junit.Test;
+import org.mockito.Matchers;
 import org.mockito.Mockito;
 
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class SoccerDataObjectNotifiesDisplaysTest {
     private Display mockDisplay = Mockito.mock(Display.class);
     private Display mockDisplay2 = Mockito.mock(Display.class);
-    private Display mockDisplay3 = Mockito.mock(Display.class);
 
     @Test
     public void shallNotifyASingleDisplayOnMeasurementsChanged() throws Exception {
@@ -27,13 +30,25 @@ public class SoccerDataObjectNotifiesDisplaysTest {
         SoccerDataObject soccerDataObject = new SoccerDataObject();
         soccerDataObject.appendDisplay(mockDisplay);
         soccerDataObject.appendDisplay(mockDisplay2);
-        soccerDataObject.appendDisplay(mockDisplay3);
 
         soccerDataObject.measurementsChanged();
 
         verify(mockDisplay).onMeasurementsChanged(soccerDataObject);
         verify(mockDisplay2).onMeasurementsChanged(soccerDataObject);
-        verify(mockDisplay3).onMeasurementsChanged(soccerDataObject);
+    }
+
+    @Test
+    public void shallAlwaysNotifyAllDisplaysInCaseOfDisplayException() throws Exception {
+        SoccerDataObject soccerDataObject = new SoccerDataObject();
+        soccerDataObject.appendDisplay(mockDisplay);
+        soccerDataObject.appendDisplay(mockDisplay2);
+        doThrow(new RuntimeException()).when(mockDisplay).onMeasurementsChanged(Matchers.<SoccerDataObject>anyObject());
+        doThrow(new RuntimeException()).when(mockDisplay2).onMeasurementsChanged(Matchers.<SoccerDataObject>anyObject());
+
+        soccerDataObject.measurementsChanged();
+
+        verify(mockDisplay).onMeasurementsChanged(soccerDataObject);
+        verify(mockDisplay2).onMeasurementsChanged(soccerDataObject);
     }
 
 }
